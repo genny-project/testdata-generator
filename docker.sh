2fbdbfdd1372b01ad10d3edc4a8cad0515a5ce98
rzldev1by1 <amrizal.fajar@onebyone.co>
2022-11-19 08:55:49 +0800

    docker still not working

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 clear
-docker-compose -f docker-compose-development.yml down
-docker volume rm genny-data-generator_infinispan_mysql
-docker-compose -f docker-compose-development.yml up
+docker-compose -f docker-compose-dev.yml down
+docker volume rm genny-data-generator_mysqldb
+docker-compose -f docker-compose-dev.yml up
